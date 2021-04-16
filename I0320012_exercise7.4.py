ab6c1bf5c7955144074e24e00e4391a4b876e027
ardaneshwara <80144925+ardaneshwara@users.noreply.github.com>
2021-04-16 21:12:07 +0700

    Delete I0320012_exercise7.4.py

--- I0320012_exercise7.4.py
+++
@@ -1,9 +0,0 @@
-def panggil(func):
-    return func
-def helloworld():
-    return "HELLO WORLD"
-def main():
-    s = panggil(helloworld())
-    print(s)
-    if _name_ == '_main_' :
-        main()
